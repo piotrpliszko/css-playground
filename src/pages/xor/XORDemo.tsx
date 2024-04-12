9a3c931db3ea42dc651a9947819bfa0a665c2934
Piotr Pliszko <pliszko.piotr@gmail.com>
2024-04-12 20:13:19 +0200

    feat: basic XOR effect

--- src/pages/xor/XORDemo.tsx
+++ src/pages/xor/XORDemo.tsx
@@ -5,7 +5,10 @@ export const XORDemo: FunctionComponent = () => {
   return (
     <div>
       <h1>XOR demo</h1>
-      <div className="container"></div>
+      <div className="container">
+        <div className="box"></div>
+        <div className="text">XOR</div>
+      </div>
     </div>
   );
 };
